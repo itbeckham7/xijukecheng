b59e7aa4ed5055ae5494fc383adbd7508bc459ff
ExIdeaTech Jiao Wei <pandaworld1985@outlook.com>
2020-09-03 22:30:28 +0800

    0903-CCJ-update grammar part

--- assets/js/custom/home.js
+++ assets/js/custom/home.js
@@ -16,18 +16,18 @@ exitBtn.mouseover(function () {
 exitBtn.mouseout(function () {
     exitBtn.css({"background":"url("+imageDir+"base/exit.png) no-repeat","background-size" : "100% 100%"});
 });
-$('#sh_kebenju_btn').mouseover(function(){
-   $(this).css({width:'31.02%',height:'71.44%',top:'19.28%',left:'10.7%'});
-});
-$('#sh_kebenju_btn').mouseout(function(){
-    $(this).css({width:'29.2%',height:'69.44%',top:'20.78%',left:'11.7%'});
-});
-$('#sh_sandapian_btn').mouseover(function(){
-    $(this).css({width:'31.2%',height:'71.51%',top:'19.87%',left:'56.88%'});
-});
-$('#sh_sandapian_btn').mouseout(function(){
-    $(this).css({width:'29.2%',height:'69.51%',top:'20.87%',left:'57.88%'});
-});
+// $('#sh_kebenju_btn').mouseover(function(){
+//    $(this).css({width:'31.02%',height:'71.44%',top:'19.28%',left:'10.7%'});
+// });
+// $('#sh_kebenju_btn').mouseout(function(){
+//     $(this).css({width:'29.2%',height:'69.44%',top:'20.78%',left:'11.7%'});
+// });
+// $('#sh_sandapian_btn').mouseover(function(){
+//     $(this).css({width:'31.2%',height:'71.51%',top:'19.87%',left:'56.88%'});
+// });
+// $('#sh_sandapian_btn').mouseout(function(){
+//     $(this).css({width:'29.2%',height:'69.51%',top:'20.87%',left:'57.88%'});
+// });
 
 function hover_back() {
     $('#back_btn_image').attr('src', baseURL + 'assets/images/frontend/studentwork/back_hover.png');
